7eeb0a9ad8ecd78ea79397776404b57419398ed1
Alex Aralis <alex.aralis@gmail.com>
2017-04-22 19:50:34 -0400

    finish counting app

--- main.elm
+++ main.elm
@@ -1,31 +1,38 @@
-import Html exposing (..)
+import Html.Events exposing (..)
+import Html exposing (Html, button, text, div)
 
-type Msg = Reset | Increment Integer | Decrement Integer
-type alias Model = { counter1: Integer, counter2: Integer }
+type Msg = Reset | Increment Int | Decrement Int
+type alias Model = { counter1: Int, counter2: Int }
+
+main : Program Never Model Msg
+main = Html.beginnerProgram { model = { counter1 = 0, counter2 = 0 }, view = view, update = update }
 
 update : Msg -> Model -> Model
 update msg model = 
   case msg of
-    Reset -> { model | counter1 = 0, counter2 0 }
+    Reset -> { model | counter1 = 0, counter2 = 0 }
 
     Increment n -> 
       case n of 
         1 -> { model | counter1 = model.counter1 + 1 }
         2 -> { model | counter2 = model.counter2 + 1 }
+        otherwise -> model
     
     Decrement n -> 
       case n of 
         1 -> { model | counter1 = model.counter1 - 1}
         2 -> { model | counter2 = model.counter2 - 1}
+        otherwise -> model
 
 
 view : Model -> Html Msg
 view model =
   div [] 
-    [button [onClick Increment 1] [text "inc1"]
-    ,button [onClick Decrement 1] [text "dec1"]
-    ,button [onClick Increment 2] [text "inc2"]
-    ,button [onClick Decrement 2] [text "dec2"]
+    [button [onClick (Increment 1)] [text "inc1"]
+    ,button [onClick (Decrement 1)] [text "dec1"]
+    ,button [onClick (Increment 2)] [text "inc2"]
+    ,button [onClick (Decrement 2)] [text "dec2"]
+    ,button [onClick Reset] [text "reset"]
     ,text <| toString model.counter1
     ,text <| toString model.counter2
     ]
