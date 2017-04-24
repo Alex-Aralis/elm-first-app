fd58160d7a00b088173bdfe97f6f5417e1921d1a
Alex Aralis <alex.aralis@gmail.com>
2017-04-24 16:48:07 -0400

    finish basic setup of http.elm

--- http.elm
+++ http.elm
@@ -1,6 +1,8 @@
 import Html exposing (Html, div, img, text, button, h2)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (src)
+import Http
+import Json.Decode as Decode
 
 main = Html.program {
     subscriptions = always Sub.none,
@@ -10,10 +12,11 @@ main = Html.program {
   }
 
 type alias Model = 
-  {
-    gifUrl: String,
-    topic: String
-  }
+    {
+      gifUrl: String,
+      topic: String
+    }
+
 
 init : (Model, Cmd Msg)
 init = 
@@ -25,12 +28,29 @@ init =
     Cmd.none
   )
 
-type Msg = MorePls
+type Msg = 
+    MorePls
+  | NewGif (Result Http.Error String)
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
   case msg of 
-    MorePls -> (model, Cmd.none)
+    MorePls -> (model, getGifUrl model.topic)
+
+    NewGif (Ok url) -> ({ model | gifUrl = url }, Cmd.none)
+
+    NewGif (Err _) -> (model, Cmd.none)
+
+
+getGifUrl : String -> Cmd Msg
+getGifUrl topic = 
+  let
+    getUrl = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+    gifDecoder = Decode.at ["data", "image_url"] Decode.string
+    request = Http.get getUrl gifDecoder
+  in
+    Http.send NewGif request
 
 view : Model -> Html Msg
 view model =
